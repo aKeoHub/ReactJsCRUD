2ac207e8dcd0fd661e52b43f3df8e064afbe0fc4
Andy Keobounphan <andykeo@live.ca>
2022-07-27 12:15:31 -0600

    Initial Commit.

---
+++ server/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'demo',
+});
+
+app.post('/register', (req, res) => {
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    db.query(
+        'INSERT INTO user (userName, password) VALUES (?,?)', [username, password],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Values inserted');
+
+            }
+        }
+    );
+});
+
+app.get('/users', (req, res) => {
+    db.query('SELECT * FROM user', (err, result) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.send(result);
+        }
+    });
+});
+app.listen(3001, () => {
+    console.log('Server running on port 3001!');
+});
---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react';
+import './App.css';
+import Axios from 'axios';
+
+
+function App() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [userList, setUserList] = useState([]);
+
+
+    const getUsers = () => {
+        Axios.get('http://localhost:3001/users', {}).then((response) => {
+            setUserList(response.data)
+        });
+    };
+
+    const register = () => {
+        Axios.post('http://localhost:3001/register', {
+            username: username,
+            password: password,
+        }).then(() => {
+            console.log("Success");
+        });
+    };
+
+    return <div className="App">
+        <h1>LOGIN BABY</h1>
+
+
+        <div className="registration">
+            <h1>Registration</h1>
+            <label>Username:</label>
+            <input type="text" onChange={(e) => {
+                setUsername(e.target.value);
+            }}
+            />
+            <label>Password:</label>
+            <input type="text" onChange={(e) => {
+                setPassword(e.target.value);
+            }}
+            />
+            <button onClick={register}>Register</button>
+        </div>
+        ---------------------------------------------------------------------------------------------------------
+
+        <div className="users">
+            <button onClick={getUsers}>Show Users</button>
+
+            {userList.map((val, key) => {
+                return <div key={key} className="user">
+                    <h3>Username: {val.userName + ' Password: ' + val.password}</h3>
+                </div>
+            })}
+        </div>
+    </div>
+
+}
+
+export default App;
